32e53ce61b1ed3006a4442cafce0830c5c04d085
jakebark <jacob.barker@gmail.com>
2026-02-11 12:00:54 +0000

    split linux into debian, add rhel

--- ansible-debian.sh
+++ ansible-debian.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Debian/Ubuntu ansible setup
 apt update
 apt install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
---
+++ ansible-rhel.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# RHEL/CentOS/Rocky/AlmaLinux ansible setup
+sudo yum update -y
+sudo yum install -y epel-release
+sudo yum install -y ansible
